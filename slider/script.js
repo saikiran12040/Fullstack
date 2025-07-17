7ada3c8b74f13f1b5049987a33ef6858f9be8f48
Sai Kiran <saikiran12040@gmail.com>
2025-07-17 18:22:32 +0530

    added

---
+++ slider/script.js
@@ -0,0 +1,15 @@
+const threeDots=document.querySelector(".three-dots")
+const container=document.querySelector(".container")
+
+
+threeDots.addEventListener('click',()=>{
+    container.style.width='50vw'
+    threeDots.classList.toggle('hidden')
+})
+
+const cut=document.querySelector(".cut")
+
+cut.addEventListener('click',()=>{
+    container.style.width='0'
+    threeDots.classList.toggle('hidden')
+})
