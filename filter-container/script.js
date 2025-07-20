cd034b6b76625adc7de17203310dffd9e89162db
Sai Kiran <saikiran12040@gmail.com>
2025-07-20 15:07:48 +0530

    added

---
+++ filter-container/script.js
@@ -0,0 +1,40 @@
+const productList = document.getElementById("product-list");
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const categoryBtns = document.querySelectorAll(".category-btn");
+
+function filterProduct() {
+  let searchValue = searchInput.value.toLowerCase();
+  let productItems = document.querySelectorAll(".product-item");
+  let activeBtn = document.querySelector(".category-btn.active");
+  let activeCategory = activeBtn ? activeBtn.dataset.category : "all";
+
+  productItems.forEach((item) => {
+    const title = item.querySelector("h3").innerText.toLowerCase();
+    const category = item.dataset.category;
+
+    if (
+      (title.includes(searchValue) || searchValue == "") &&
+      (category === activeCategory || activeCategory == "all")
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function setCategory(e) {
+  categoryBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  e.target.classList.add("active");
+  filterProduct();
+}
+
+searchBtn.addEventListener("click", filterProduct);
+searchInput.addEventListener("keyup", filterProduct);
+categoryBtns.forEach((btn) => {
+  btn.addEventListener("click", setCategory);
+});
+filterProduct();
